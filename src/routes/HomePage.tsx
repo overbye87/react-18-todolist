542b2e4adf623f57dd6f5cc1ed49660a5afe7996
overbye <overbye87@gmail.com>
2022-04-22 19:08:46 +0300

    add matrix filter

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Select from 'react-select';
+import matrix from '../matrix.json'
+
+const levelOptions = [
+  { label: 'junior', value: 'junior' },
+  { label: 'middle', value: 'middle' },
+  { label: 'senior', value: 'senior' },
+]
 
 const Home = () => {
+  const [levelArr, setLevelArr] = useState([levelOptions[0].value])
   return (
-    <div>Home</div>
+    <>
+      <Select
+        className="select"
+        options={levelOptions}
+        isMulti
+        onChange={(value) => setLevelArr(value ? value.map((item)=>item.value) : [])}
+        defaultValue={levelOptions[0]}
+      />
+      <ul>
+        {matrix.data.map((item) =>
+          <li key={item.id}>
+            {item.id}
+            {' '}
+            {item.title}
+            <ul>
+              {item.matrix_sections.map((item) =>
+                <li>
+                  {item.title}
+                  {' '}
+                  <ul>
+                    {item.matrix_skills.map((item) =>
+                      levelArr.map((level) => {
+                        if (item.level === level)
+                          return <li>
+                            {item.title}
+                            <div dangerouslySetInnerHTML={{ __html: item.description }}></div>
+                          </li>
+                        else return null
+                      })
+                    )}
+                  </ul>
+                </li>
+              )}
+            </ul>
+
+          </li>
+        )}
+      </ul>
+    </>
   )
 }
 
