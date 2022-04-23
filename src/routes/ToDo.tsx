d0310af38b2126bcc70009097a53a5c99e6343bb
overbye <overbye87@gmail.com>
2022-04-23 23:11:14 +0300

    rename

---
+++ src/routes/ToDo.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import AddForm from '../components/AddForm'
+import { options } from '../components/constants';
+import Filter from '../components/Filter';
+import ToDoList from '../components/ToDoList';
+import { ITodos } from '../types';
+import { useTypedSelector } from '../store/hooks';
+
+const ToDo = () => {
+
+  const todos = useTypedSelector((store) => store.todos)
+  const filter = useTypedSelector((store) => store.filter.value)
+
+  const filterTodos = (todos: ITodos, filter: string) => {
+    switch (filter) {
+      case options[1].value:
+        return todos.filter((todo) => todo.isDone === true)
+      case options[2].value:
+        return todos.filter((todo) => todo.isDone === false)
+      default:
+        return todos
+    } 
+  }
+
+  return (
+    <>
+      <h1>ToDo</h1>
+      <div className="container">
+        <AddForm />
+        <Filter />
+        <ToDoList
+          todos={filterTodos(todos, filter)}
+        />
+      </div>
+    </>
+  )
+}
+
+export default ToDo
